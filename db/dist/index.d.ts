5b258ea2d94ac696c24e2450711dc8f1cf12b87e
Ruchir28 <ruchirnakra28@gmail.com>
2024-10-22 13:20:34 +0530

    SmartWallet ci setup

--- db/dist/index.d.ts
+++ db/dist/index.d.ts
@@ -1,8 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-import prisma from '../prisma';
-declare const db: {
-    prisma: PrismaClient;
-    disconnect: () => Promise<void>;
-};
-export default db;
-export * from '@prisma/client';
+import { PrismaClient as SmartWalletClient } from '@prisma/client-smart-wallet';
+import { PrismaClient as UserClient } from '@prisma/client-telbot';
+declare const smartWalletClient: SmartWalletClient<import("@prisma/client-smart-wallet").Prisma.PrismaClientOptions, never, import("@prisma/client-smart-wallet/runtime/library").DefaultArgs>;
+declare const telbotClient: UserClient<import("@prisma/client-telbot").Prisma.PrismaClientOptions, never, import("@prisma/client-telbot/runtime/library").DefaultArgs>;
+export { smartWalletClient, telbotClient };
--- db/dist/index.js
+++ db/dist/index.js
@@ -1,32 +1,9 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-// Create a single instance of the Prisma client
-const prisma = new client_1.PrismaClient();
-// Create an object with database-related functions
-const db = {
-    // Expose the Prisma client instance
-    prisma,
-    // Function to disconnect the Prisma client
-    async disconnect() {
-        await prisma.$disconnect();
-    },
-};
-// Export the db object
-exports.default = db;
-// Optionally, export types if needed
-__exportStar(require("@prisma/client"), exports);
+exports.telbotClient = exports.smartWalletClient = void 0;
+const client_smart_wallet_1 = require("@prisma/client-smart-wallet");
+const client_telbot_1 = require("@prisma/client-telbot");
+const smartWalletClient = new client_smart_wallet_1.PrismaClient();
+exports.smartWalletClient = smartWalletClient;
+const telbotClient = new client_telbot_1.PrismaClient();
+exports.telbotClient = telbotClient;
